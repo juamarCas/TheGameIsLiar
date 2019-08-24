1a3d573049e48a8352406304c514120a23b19a05
Juan David <juanmartincas@gmail.com>
2019-08-24 18:56:57 -0500

    estupido

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    private CharacterController controller;
+    public Rigidbody rb;
+    private float lastRotation; 
+    private Vector3 moveDirection;
+    public float rotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMove();
+        PlayerRotate();
+    }
+
+    private void PlayerMove()
+    {
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+    }
+
+    #region Rotation
+    private void PlayerRotate()
+    {
+        if(Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            Rotate(-90);    
+        }
+        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            Rotate(90);
+        }else if(!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            Rotate(0);
+        }else if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S))
+        {
+            Rotate(180);
+        }else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            Rotate(45);
+        }
+        else if (!Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S))
+        {
+            Rotate(-135);
+        }else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S))
+        {
+            Rotate(135);
+        }else if (!Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+        {
+            Rotate(-45);
+        }
+        else
+        {
+            Rotate(lastRotation);
+        }
+    }
+    private void Rotate(float y)
+    {
+        this.gameObject.transform.rotation = Quaternion.Euler(0f, y , 0f);
+        lastRotation = y; 
+    }
+    #endregion
+
+}
